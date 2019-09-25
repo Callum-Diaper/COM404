c39ccf2cff612a14446736623b53b0efe8c5f60b
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-09-25 10:12:52 +0000

    Added Input folder and complete tasks 1&2

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,14 @@
+beep = input("Please enter a character for Beep's eyes ")
+
+
+
+print("     ________")
+print("    /________\ ")
+print("   /| "+ beep +"   "+ beep +"  |\ ")
+print("   ||________||")
+print("    \________/")
+print("    (  _____  )")
+print("    /|'      |\ ")
+print("   / |       | \ ")
+print("  /  |       |  \ ")
+print("      \_____/")
