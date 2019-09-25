7b583b607603e607c6a9edb771fe27f9d3c74152
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-09-25 10:40:26 +0000

    Completed task 4. Added adjustable lives, energy, and shield levels that then display characters depending on input.

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,11 @@
+lives = int(input("Please enter the number of lives "))
+energy = int(input("Please enter the energy level "))
+shield = int(input("Plese enter the shield level "))
+
+lives_level = "♥"*lives
+energy_level = "♦"*energy
+shield_level = "♦"*shield
+
+print("Lives:", lives_level)
+print("Energy:", energy_level)
+print("Shield:", shield_level)
