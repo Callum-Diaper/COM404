c1d4e1fc50270790602dc20ab86d7d37d8317edf
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-02 08:56:02 +0000

    Added if-else statement

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,8 @@
+user_inp = str(input("Please enter the activity to be performed "))
+
+if user_inp == "calculate":
+    print("Perofrming calculations...")
+else:
+    print("Performing activity...")
+
+print("Activity completed!")
