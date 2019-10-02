2ec0a739f1265c49bde79973101ae1e63985b14d
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-02 09:19:56 +0000

    added number comparison to see which is largest

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,9 @@
+first_num = int(input("Please enter your first number "))
+sec_num = int(input("Please enter your second number "))
+
+if first_num > sec_num:
+    print("The first number is bigger!")
+elif sec_num > first_num:
+    print("The second number is bigger!")
+else:
+    print("Invalid input")
