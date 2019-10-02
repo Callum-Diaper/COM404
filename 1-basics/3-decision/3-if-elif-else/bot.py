32faad4e392f525f1b5368b1d80136810934e7ec
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-02 09:02:04 +0000

    Added beep painting if-elif-else tasks

---
+++ 1-basics/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,14 @@
+user_inp = str(input("Towards which direction should I paint? (left, right, up or down) "))
+
+if user_inp == "up":
+    print("I am painting in the upward direction!")
+elif user_inp == "down":
+    print("I am painting in the downwards direction!")
+elif user_inp == "left":
+    print("I am painting in the leftward direction!")
+elif user_inp == "right":
+    print("I am painting in the rightward direction!")
+else:
+    print("I do not know that direction!")
+
+    
