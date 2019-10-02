e204497378607fd767a088016b7a4e74ed6a0d90
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-02 09:51:43 +0000

    Added task 10 with the and operator

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,7 @@
+sound_inp = str(input("What sound did I just hear? "))
+sight_inp = str(input("What did I just see? "))
+
+if (sound_inp == "grrr") and (sight_inp == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")
