fc34af4d432e886c3785e88a7d1ed67b2110cc4f
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-09 10:52:36 +0000

    Added for loop to print mountains

---
+++ 1-basics/4-repetition/2-for-loops/1-simple/bot.py
@@ -0,0 +1,8 @@
+user_inp = int(input("How many mountains should I display? "))
+for i in range (user_inp):
+    print("           __")
+    print("          /  \_  ")
+    print("         /^    \ ")
+    print("        /  ^    \_")
+    print("      _/ ^ ^     ^\ ")
+    print("     /  ^     ^    \ ")
