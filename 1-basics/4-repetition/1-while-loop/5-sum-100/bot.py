7cc42fd8bb5f42c5dd5d791a568b8d19b469b4bf
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-09 09:54:09 +0000

    Added a while loop to calculate the sum of the first 100 numbers

---
+++ 1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,9 @@
+print("Calculating the sum of the first 100 numbers...")
+
+total = 0
+iteration = 0
+while (iteration < 100):
+    iteration = iteration + 1
+    total = total + iteration
+    print(total)
+print("...Done! The result is",total)
