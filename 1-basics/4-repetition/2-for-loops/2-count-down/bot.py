b6d8a86261fcf02d2ca32c083a2970aa69ec5dd1
Callum-Diaper <4DIAPC72@solent.ac.uk>
2019-10-09 10:59:29 +0000

    Added a for loop that counts backwards

---
+++ 1-basics/4-repetition/2-for-loops/2-count-down/bot.py
@@ -0,0 +1,6 @@
+user_inp = int(input("How many steps are we from the cave? "))
+stop = (user_inp - user_inp)
+
+for i in range (user_inp, stop, -1):
+    print(i,"Steps remaining")
+print("We have reached the cave!")
